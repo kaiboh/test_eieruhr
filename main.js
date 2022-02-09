c3b26f0a52541e8c64ba977a0b274f8a11165ba7
kaiboh <99281522+kaiboh@users.noreply.github.com>
2022-02-09 04:20:15 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict"
+
+let alarm = new Audio('alarm.wav');
+let timerStarted = false;
+
+function startTimer() { 
+    if (!timerStarted) {
+        let startTime = new Date().getTime();
+        let fiveMinutes = 1000 * 60 * 5;
+        let endTime = startTime + fiveMinutes;
+
+        setInterval(function() { 
+            let timeLeft = endTime - new Date().getTime();
+
+            if (timeLeft > 0) {
+                let minutes = timeLeft / (1000 * 60); 
+                minutes = Math.floor(minutes);
+                let seconds = (timeLeft / 1000) % 60; 
+                seconds = Math.round(seconds); 
+                seconds = ('o' + seconds).slice(-2);
+                let text = '0' + minutes + ' : ' + seconds; 
+                timer.innerHTML = text; 
+            } else {
+                alarm.play();
+                timer.innerHTML = '00 : 00';
+            }
+        
+        }, 1000)
+        timerStarted = true;
+    }   
+}   
