96825749b3d9a00d06852f5fe0b3c75290344b93
kai bohstedt <kai.bohstedt@axians.at>
2022-02-10 02:54:09 -0800

    next fixes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const alarm = new `audio`('alarm.wav');
+const alarm = new 'audio'('alarm.wav');
 let timerStarted = false;
 
 function startTimer() {
@@ -9,8 +9,8 @@ function startTimer() {
         const fiveMinutes = 1000 * 60 * 5;
         const endTime = startTime + fiveMinutes;
 
-        setInterval (function() {
-            let timeLeft = endTime - new Date().getTime();
+        setInterval(function () {
+            const timeLeft = endTime - Date.now().getTime();
 
             if (timeLeft > 0) {
                 let minutes = timeLeft / (1000 * 60);
@@ -18,13 +18,10 @@ function startTimer() {
                 let seconds = (timeLeft / 1000) % 60;
                 seconds = Math.round(seconds);
                 seconds = ('o' + seconds).slice(-2);
-                let text = '0' + minutes + ' : ' + seconds;
+                const text = '0' + minutes + ' : ' + seconds;
                 timer.innerHTML = text;
                 timerStarted = true;
             } else {
                 alarm.play();
                 timer.innerHTML = '00 : 00';
-}
-}, 1000);
-}
-}
+}}, 1000);}}
