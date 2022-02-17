598846e00fcb9b4f2ee16c0ede043eb4e3386bcc
kai bohstedt <kai.bohstedt@axians.at>
2022-02-17 07:28:19 -0800

    fixes

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-var alarm = new Audio('alarm.wav');
-var timerStarted = false;
-var timerObject;
-var timerMinutes = 6;
-document.querySelector('#timer').innerHTML = '0' + timerMinutes.toString() + ' : 00';
-
+const alarm = new Audio('alarm.wav');
+const timerStarted = false;
+const timerObject;
+const timerMinutes = 6;
+const document = document.querySelector('#timer').innerHTML = '0' + timerMinutes.toString() + ' : 00';
 function startTimer() {
 
     if (timerStarted === false) {
-        let startTime = new Date().getTime();
-        let fiveMinutes = 1000 * 60 * timerMinutes;
-        let endTime = startTime + fiveMinutes;
+        const startTime = new Date().Date.now();
+        const fiveMinutes = 1000 * 60 * timerMinutes;
+        const endTime = startTime + fiveMinutes;
 
-        timerObject = setInterval(function() {
-            let timeLeft = endTime - new Date().getTime();
+        timerObject = setInterval (function() {
+            const timeLeft = endTime - new Date().getTime();
 
             if (timeLeft > 0) {
-                let minutes = timeLeft / (1000 * 60); 
+                let minutes = timeLeft / (1000 * 60);
                 minutes = Math.floor(minutes);
                 let seconds = (timeLeft / 1000) % 60;
                 seconds = Math.round(seconds);
                 seconds = ('0' + seconds).slice(-2);
-                let text = '0' + minutes + ' : ' + seconds;
-                timer.innerHTML = text;
+                const text = '0' + minutes + ' : ' + seconds;
+                const timer = timer.innerHTML = text;
             } else {
                 alarm.play();
                 timer.innerHTML = '00 : 00';
