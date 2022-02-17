00da03ca80bb2804e38f1a7d21566e88c32f3599
kai bohstedt <kai.bohstedt@axians.at>
2022-02-17 05:21:45 -0800

    update main.js style.css index.html

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
+var alarm = new Audio('alarm.wav');
+var timerStarted = false;
+var timerObject; 
+var timerMinutes = 6;
+document.getElementById('timer').innerHTML = '0' + timerMinutes.toString() + ' : 00';
 
-const alarm = new 'audio'('alarm.wav');
-let timerStarted = false;
-(function startTimer() {
-    if (!timerStarted) {
-        const startTime = new 'Date'().getTime();
-        const fiveMinutes = 1000 * 60 * 5;
-        const endTime = startTime + fiveMinutes;
-        setInterval(function () {
-            const timeLeft = endTime - Date.now().getTime();
+function startTimer() { 
+
+    if (timerStarted == false) {
+        let startTime = new Date().getTime();
+        let fiveMinutes = 1000 * 60 * timerMinutes;
+        let endTime = startTime + fiveMinutes;
+
+        timerObject = setInterval(function() { 
+            let timeLeft = endTime - new Date().getTime();
+
             if (timeLeft > 0) {
-                let minutes = timeLeft / (1000 * 60);
+                let minutes = timeLeft / (1000 * 60); 
                 minutes = Math.floor(minutes);
-                let seconds = (timeLeft / 1000) % 60;
-                seconds = Math.round(seconds);
-                seconds = ('o' + seconds).slice(-2);
-                const text = '0' + minutes + ' : ' + seconds;
-                timer.innerHTML = text;
-                timerStarted = true;
+                let seconds = (timeLeft / 1000) % 60; 
+                seconds = Math.round(seconds); 
+                seconds = ('0' + seconds).slice(-2);
+                let text = '0' + minutes + ' : ' + seconds; 
+                timer.innerHTML = text; 
             } else {
                 alarm.play();
                 timer.innerHTML = '00 : 00';
-                }
+            }
+        
         }, 1000);
-    }
-})();
+        document.getElementById("startButton").innerHTML = "timer started - click to stop"
+        timerStarted = true;
+    }  
+    else
+    {
+        clearInterval(timerObject);
+        timerStarted = false;
+        document.getElementById('timer').innerHTML = '0' + timerMinutes.toString() + ' : 00';
+
+        document.getElementById("startButton").innerHTML = "start"
+    } 
+}
